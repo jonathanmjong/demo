2220e14a250111386cef6e9f6d5dfc57f5d4ad32
Jonathan Jong <jonathanjong@jonathans-air.lan>
2022-02-01 20:51:27 -0600

    added EJS

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-
 app.get('/', (req, res) => {
-    res.sendFile('index.html', () => {
-        console.log('user on home page');
-    });
+    console.log("at home site");
+
+    res.render('index', {fullName: ""});
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body.firstName, req.body.lastName, req.body.email);
-    res.redirect('index.html');
-})
+    const fullName = req.body.firstName + ' ' + req.body.lastName;
+    res.render('index', {fullName: fullName});
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server running");
