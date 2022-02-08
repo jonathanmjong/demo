a8f1ec31cef3ade11805d96bec7632654aece6b2
Jonathan Jong <jonathanjong@jonathans-air.lan>
2022-02-07 21:59:03 -0600

    cleanup

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+// .ejs pages need to be stored in "views" folder
 app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.get('/', (req, res) => {
-    console.log("at home site");
+    console.log("GET /");
 
-    res.render('index', {fullName: ""});
+    res.render('index', {fullName: "test name"});
 });
 
 app.post('/', (req, res) => {
+    console.log('POST /')
     const fullName = req.body.firstName + ' ' + req.body.lastName;
     res.render('index', {fullName: fullName});
 });
